004511d9b75bbe1e38765c0f855382885de686aa
Nina ten Pas <pnl0tg6l@Emea.Royalahold.net>
2023-01-27 11:52:08 +0100

    3 persons

--- exercises/exercise_1/opdracht_1.py
+++ exercises/exercise_1/opdracht_1.py
@@ -1,10 +1,12 @@
 def likes(team: list) -> str:
-    if len(team) > 2:
+    if len(team) > 3:
         print("Error: max 2 names allowed")
     elif len(team) == 1:
         print("{} likes this!".format(team[0]))
     elif len(team) == 2:
         print("{} and {} like this!".format(team[0], team[1]))
+    elif len(team) == 3:
+        print("{}, {} and {} like this!".format(team[0], team[1], team[2]))
     else:
-        print("no one likes this")
+        print("no one likes this!")
 
