67143883330f465ffe854b17cf3628ff1bdf0183
Klinkenberg <pnl07h5x@Emea.Royalahold.net>
2023-01-27 11:42:04 +0100

    eerste deel opdracht 1

--- exercises/exercise_1/opdracht_1.py
+++ exercises/exercise_1/opdracht_1.py
@@ -1,2 +1,10 @@
 def likes(team: list) -> str:
-    return True
+    if len(team) > 2:
+        print("Error: max 2 names allowed")
+    elif len(team) == 1:
+        print("{} likes this!".format(team[0]))
+    elif len(team) == 2:
+        print("{} and {} like this!".format(team[0], team[1]))
+    else:
+        print("no one likes this")
+
